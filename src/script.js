abcec3cd5dd243c4eb90a746aa5850df6eea2503
P SIDHARTH <philkhanasidharth14@gmail.com>
2024-08-04 13:47:50 +0530

    init
    Signed-off-by: P SIDHARTH <philkhanasidharth14@gmail.com>

---
+++ src/script.js
@@ -0,0 +1,45 @@
+async function fetchLink() {
+  const response = await fetch("/get-link");
+  const data = await response.json();
+  if (data.link) {
+    const btn = document.getElementById("btn");
+    btn.innerHTML = "Join In";
+    btn.disabled = false;
+    btn.onclick = () => {
+      window.open(data.link, "_blank");
+    };
+  }
+}
+
+function startCountdown() {
+  const now = new Date();
+  const targetTime = new Date();
+  targetTime.setHours(18, 0, 0, 0);
+
+  if (now.getTime() > targetTime.getTime()) {
+    targetTime.setDate(targetTime.getDate() + 1);
+  }
+
+  const interval = setInterval(() => {
+    const currentTime = new Date().getTime();
+    const distance = targetTime - currentTime;
+
+    const hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById(
+      "countdown"
+    ).innerHTML = `${hours}h ${minutes}m ${seconds}s`;
+
+    if (distance < 0) {
+      clearInterval(interval);
+      document.getElementById("countdown").innerHTML = "Countdown Finished";
+      fetchLink();
+    }
+  }, 1000);
+}
+
+window.onload = startCountdown;
